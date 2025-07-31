fc70b612534badfff501cb53fefee2bbc8858abf
Sandiso2080 <91184967+Sandiso2080@users.noreply.github.com>
2025-07-31 16:58:35 +0200

    Add project files.

---
+++ DiaryEntry/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DiaryEntry.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryEntry.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DiaryModel> DiaryEntries { get; set; }
+    }
+
+}
---
+++ DiaryEntry/Models/DiaryModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryEntry.Models
+{
+    public class DiaryModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
